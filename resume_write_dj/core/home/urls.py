d5fb8bd56a244f8318b43d9c4a9dfd8a97de2ec0
sidharth bakare <sidharthbakare7@gmail.com>
2024-10-25 13:57:33 +0530

    resume writer

---
+++ resume_write_dj/core/home/urls.py
@@ -0,0 +1,12 @@
+
+from django.contrib import admin 
+from django.urls import path 
+from home.views import *
+
+urlpatterns = [ 
+	path('home/', home, name = 'home'), 
+	path('resume/', gen_resume, name = 'resume'), 
+	path("admin/", admin.site.urls), 
+] 
+
+
